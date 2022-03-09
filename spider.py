d8d7d6d792984dc43af4d1ee4f1091941627b87a
user <totoroliu0929@gmail.com>
2022-03-09 14:36:25 +0800

    modify

--- spider.py
+++ spider.py
@@ -6,6 +6,15 @@ soup = BeautifulSoup(r.text,"html.parser")
 u = soup.select("#main-2-QuoteDividend-Proxy .table-body-wrapper li div.Ta\(start\)")
 print(u[0])
 print(len(u))
+u2 = soup.select("#main-2-QuoteDividend-Proxy .table-body-wrapper li.List\(n\) div:nth-of-type(2) span")
+print(u2[0])
+print(len(u2))
+
+u3 = soup.select("#main-2-QuoteDividend-Proxy .table-body-wrapper li.List\(n\) div:nth-of-type(3) span")
+print(u3[0])
+print(len(u3))
+for item in u2:
+    print(item.text)
 
 """
 r = requests.get("https://www.ptt.cc/bbs/joke/index.html")
