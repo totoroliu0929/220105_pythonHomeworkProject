ffb2fb3e233e4b9c890692756a4e1839c69e9575
user <totoroliu0929@gmail.com>
2022-03-10 16:48:42 +0800

    modify

--- spider.py
+++ spider.py
@@ -5,8 +5,19 @@ import urllib.request, csv
 class spider:
     def __init__(self,id):
         self.id = id
+        self.profile = dict()
         self.listDividend = dict()
         self.listEps = dict()
+
+    def profile(self):
+        r = requests.get("https://tw.stock.yahoo.com/quote/{}.TW/profile".format(self.id))
+        soup = BeautifulSoup(r.text, "html.parser")
+        u1 = soup.select("#main-2-QuoteProfile-Proxy section:nth-of-type(1) span>span")
+        u2 = soup.select("#main-2-QuoteProfile-Proxy section:nth-of-type(1) span+div")
+        for i in range(len(u2)):
+            print(u2[i].text)
+            #self.profile[f"{u1[i].text}"] = u2[i].text
+        print(self.profile)
 
     def dividend(self):
         r = requests.get("https://tw.stock.yahoo.com/quote/{}.TW/dividend".format(self.id))
@@ -18,12 +29,12 @@ class spider:
             #item = [u1[i].text,u2[i].text,u3[i].text]
             #self.listDividend.append(item)
             if u1[i].text[0:4:1] in self.listDividend:
-                self.listDividend[u1[i].text[0:4:1]][1] += 1
-                self.listDividend[u1[i].text[0:4:1]][2] += float(u2[i].text)
-                self.listDividend[u1[i].text[0:4:1]][3] += float(u3[i].text)
+                self.listDividend[u1[i].text[0:4:1]]["次數"] += 1
+                self.listDividend[u1[i].text[0:4:1]]["現金股利"] += float(u2[i].text)
+                self.listDividend[u1[i].text[0:4:1]]["股票股利"] += float(u3[i].text)
                 print(u1[i].text,float(u2[i].text),float(u3[i].text))
             else:
-                self.listDividend[u1[i].text[0:4:1]] = [u1[i].text[0:4:1],1,float(u2[i].text),float(u3[i].text)]
+                self.listDividend[u1[i].text[0:4:1]] = {"年度":u1[i].text[0:4:1],"次數":1,"現金股利":float(u2[i].text),"股票股利":float(u3[i].text)}
                 print(u1[i].text, float(u2[i].text), float(u3[i].text))
         print(self.listDividend)
 
@@ -34,11 +45,11 @@ class spider:
         u2 = soup.select("#qsp-eps-table li.List\(n\)>div>div:nth-of-type(2)")
         for i in range(len(u1)):
             if u1[i].text[0:4:1] in self.listEps:
-                self.listEps[u1[i].text[0:4:1]][1] += 1
-                self.listEps[u1[i].text[0:4:1]][2] += float(u2[i].text)
+                self.listEps[u1[i].text[0:4:1]]["次數"] += 1
+                self.listEps[u1[i].text[0:4:1]]["每股盈餘"] += float(u2[i].text)
                 print(u1[i].text,float(u2[i].text))
             else:
-                self.listEps[u1[i].text[0:4:1]] = [u1[i].text[0:4:1],1,float(u2[i].text)]
+                self.listEps[u1[i].text[0:4:1]] = {"年度":u1[i].text[0:4:1], "次數":1, "每股盈餘":float(u2[i].text)}
                 print(u1[i].text, float(u2[i].text))
         print(self.listEps)
 
@@ -56,7 +67,7 @@ class spider:
         for i in data:
             print('證券代號->',i[0],'證券名稱=',i[1], '成交股數=',i[2], '成交金額=',i[3],'開盤價=',i[4], '最高價=',i[5],'最低價=',i[6], '收盤價=',i[7], '漲跌價差=',i[8], '成交筆數=',i[9])
 
-spider("2330").dividend()
+spider("2330").profile()
 """
 r = requests.get("https://www.ptt.cc/bbs/joke/index.html")
 soup = BeautifulSoup(r.text,"html.parser")
