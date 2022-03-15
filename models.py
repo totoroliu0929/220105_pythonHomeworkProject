a791397b75ae0c379aee563fbd9d5a6371cc0e61
user <totoroliu0929@gmail.com>
2022-03-15 13:17:30 +0800

    modify

--- models.py
+++ models.py
@@ -3,8 +3,9 @@ from bs4 import BeautifulSoup
 import urllib.request, csv, sqlite3, time
 from sqlite3 import Error as sqlite3Error
 
+
 class Spider:
-    def __init__(self,id:str=None):
+    def __init__(self, id: str = None):
         self.id = id
         self.listProfile = dict()
         self.listDividend = dict()
@@ -19,9 +20,32 @@ class Spider:
             u1 = soup.select("#main-2-QuoteProfile-Proxy section:nth-of-type(1) span>span")
             u2 = soup.select("#main-2-QuoteProfile-Proxy section:nth-of-type(1) span+div")
             for i in range(len(u1)):
-                #print(u1[i].text,u2[i].text)
+                # print(u1[i].text,u2[i].text)
                 self.listProfile[u1[i].text] = u2[i].text
             return self.listProfile
+        else:
+            return None
+
+    def gerDividend(self):
+        r = requests.get("https://tw.stock.yahoo.com/quote/{}.TW/dividend".format(self.id))
+        if r.status_code == 200:
+            soup = BeautifulSoup(r.text, "html.parser")
+            u1 = soup.select("#main-2-QuoteDividend-Proxy .table-body-wrapper li div.Ta\(start\)")
+            u2 = soup.select("#main-2-QuoteDividend-Proxy .table-body-wrapper li.List\(n\) div:nth-of-type(2) span")
+            u3 = soup.select("#main-2-QuoteDividend-Proxy .table-body-wrapper li.List\(n\) div:nth-of-type(3) span")
+            for i in range(len(u1)):
+                # item = [u1[i].text,u2[i].text,u3[i].text]
+                # self.listDividend.append(item)
+                if u1[i].text[0:4:1] in self.listDividend:
+                    self.listDividend[u1[i].text[0:4:1]]["次數"] += 1
+                    self.listDividend[u1[i].text[0:4:1]]["現金股利"] += float(u2[i].text)
+                    self.listDividend[u1[i].text[0:4:1]]["股票股利"] += float(u3[i].text)
+                    print(u1[i].text, float(u2[i].text), float(u3[i].text))
+                else:
+                    self.listDividend[u1[i].text[0:4:1]] = {"年度": u1[i].text[0:4:1], "次數": 1, "現金股利": float(u2[i].text),
+                                                            "股票股利": float(u3[i].text)}
+                    print(u1[i].text, float(u2[i].text), float(u3[i].text))
+            print(self.listDividend)
         else:
             return None
 
@@ -32,50 +56,40 @@ class Spider:
             u1 = soup.select("#qsp-income-statement-table .table-header-wrapper > div")
             u2 = soup.select("#qsp-income-statement-table li:nth-of-type(1) span")
             u3 = soup.select("#qsp-income-statement-table li:nth-of-type(2) span")
-            for i in range(1,len(u1)):
-                x = float(u2[i].text.replace(",", ""))
-                y = float(u3[i].text.replace(",", ""))
-                print(u1[i].text,x,y,y/x)
-        else:
-            return None
-
-
-    def gerDividend(self):
-        r = requests.get("https://tw.stock.yahoo.com/quote/{}.TW/dividend".format(self.id))
-        if r.status_code == 200:
-            soup = BeautifulSoup(r.text, "html.parser")
-            u1 = soup.select("#main-2-QuoteDividend-Proxy .table-body-wrapper li div.Ta\(start\)")
-            u2 = soup.select("#main-2-QuoteDividend-Proxy .table-body-wrapper li.List\(n\) div:nth-of-type(2) span")
-            u3 = soup.select("#main-2-QuoteDividend-Proxy .table-body-wrapper li.List\(n\) div:nth-of-type(3) span")
-            for i in range(len(u1)):
-                #item = [u1[i].text,u2[i].text,u3[i].text]
-                #self.listDividend.append(item)
-                if u1[i].text[0:4:1] in self.listDividend:
-                    self.listDividend[u1[i].text[0:4:1]]["次數"] += 1
-                    self.listDividend[u1[i].text[0:4:1]]["現金股利"] += float(u2[i].text)
-                    self.listDividend[u1[i].text[0:4:1]]["股票股利"] += float(u3[i].text)
-                    print(u1[i].text,float(u2[i].text),float(u3[i].text))
-                else:
-                    self.listDividend[u1[i].text[0:4:1]] = {"年度":u1[i].text[0:4:1],"次數":1,"現金股利":float(u2[i].text),"股票股利":float(u3[i].text)}
-                    print(u1[i].text, float(u2[i].text), float(u3[i].text))
-            print(self.listDividend)
+            for i in range(1, len(u1)):
+                quarter = u1[i].text.replace(" ", "")
+                income = int(u2[i].text.replace(",", ""))
+                grossProfit = int(u3[i].text.replace(",", ""))
+                grossMargin = grossProfit / income
+                # print(quarter,income,gross_profit,gross_margin)
+                self.listProfit[quarter] = {"quarter": quarter, "income": income, "gross_profit": grossProfit,
+                                            "gross_margin": grossMargin}
         else:
             return None
 
     def getEps(self):
+        self.getGrossMargin()
         r = requests.get("https://tw.stock.yahoo.com/quote/{}.TW/eps".format(self.id))
         soup = BeautifulSoup(r.text, "html.parser")
         u1 = soup.select("#qsp-eps-table li.List\(n\)>div>div:nth-of-type(1)")
         u2 = soup.select("#qsp-eps-table li.List\(n\)>div>div:nth-of-type(2)")
         for i in range(len(u1)):
+            quarter = u1[i].text.replace(" ", "")
+            eps = float(u2[i].text)
+            if quarter in self.listProfit:
+                self.listProfit[quarter]["EPS"] = eps
+            else:
+                self.listProfit[quarter] = {"quarter": quarter, "income": 0.0, "gross_profit": 0.0, "gross_margin": 0.0,
+                                            "EPS": eps}
             if u1[i].text[0:4:1] in self.listEps:
                 self.listEps[u1[i].text[0:4:1]]["次數"] += 1
-                self.listEps[u1[i].text[0:4:1]]["每股盈餘"] += float(u2[i].text)
-                print(u1[i].text,float(u2[i].text))
+                self.listEps[u1[i].text[0:4:1]]["每股盈餘"] += eps
+                #print(u1[i].text, eps)
             else:
-                self.listEps[u1[i].text[0:4:1]] = {"年度":u1[i].text[0:4:1], "次數":1, "每股盈餘":float(u2[i].text)}
-                print(u1[i].text, float(u2[i].text))
-        print(self.listEps)
+                self.listEps[u1[i].text[0:4:1]] = {"年度": u1[i].text[0:4:1], "次數": 1, "每股盈餘": eps}
+                #print(u1[i].text, eps)
+        #print(self.listEps)
+        return self.listProfit
 
     def getCompany(self):
         url = 'https://www.twse.com.tw/exchangeReport/BWIBBU_d?response=open_data'
@@ -86,12 +100,11 @@ class Spider:
                 continue
             if i[0] == "":
                 break
-            #print('證券代號->',i[0],'證券名稱=',i[1], '殖利率(%)=',i[2],'股利年度=',i[3], '本益比=',i[4],'股價淨值比=',i[5], '財報年/季=',i[6])
+            # print('證券代號->',i[0],'證券名稱=',i[1], '殖利率(%)=',i[2],'股利年度=',i[3], '本益比=',i[4],'股價淨值比=',i[5], '財報年/季=',i[6])
             i[2] = 0.0 if i[2] == "" else float(i[2])
             i[3] = 0.0 if i[3] == "" else float(i[3])
-            self.listCompany[i[0]] = {'id':i[0],'name':i[1],'d_yield':i[2],'PE':i[3]}
-        #self.listCompany.pop('證券代號',0)
-
+            self.listCompany[i[0]] = {'id': i[0], 'name': i[1], 'd_yield': i[2], 'PE': i[3]}
+        # self.listCompany.pop('證券代號',0)
 
     def getPrice(self):
         url = 'https://www.twse.com.tw/exchangeReport/STOCK_DAY_ALL?response=open_data'
@@ -99,20 +112,21 @@ class Spider:
         data = csv.reader(webpage.read().decode('utf-8').splitlines())
         self.getCompany()
         for i in data:
-            #print('證券代號->',i[0],'證券名稱=',i[1], '成交股數=',i[2], '成交金額=',i[3],'開盤價=',i[4], '最高價=',i[5],'最低價=',i[6], '收盤價=',i[7], '漲跌價差=',i[8], '成交筆數=',i[9])
+            # print('證券代號->',i[0],'證券名稱=',i[1], '成交股數=',i[2], '成交金額=',i[3],'開盤價=',i[4], '最高價=',i[5],'最低價=',i[6], '收盤價=',i[7], '漲跌價差=',i[8], '成交筆數=',i[9])
             if i[0] in self.listCompany:
                 i[7] = 0.0 if i[7] == "" else float(i[7])
-                self.listCompany[i[0]]['price']=i[7]
+                self.listCompany[i[0]]['price'] = i[7]
             if i[0] == "":
                 break
-        #print(self.listCompany)
+        # print(self.listCompany)
         return self.listCompany
+
 
 class Data:
     def __init__(self):
         self.listInfo = Spider().getPrice()
         self.dbFile = 'yield.db'
-        #print(self.listInfo)
+        # print(self.listInfo)
 
     def createConnection(self):
         conn = None
@@ -128,8 +142,8 @@ class Data:
         sql = '''
             CREATE TABLE IF NOT EXISTS profit_{}(
                 quarter TEXT PRIMARY KEY,
-                income REAL,
-                gross_profit REAL,
+                income INTEGER,
+                gross_profit INTEGER,
                 gross_margin REAL,
                 EPS REAL
             );
@@ -182,7 +196,7 @@ class Data:
         except sqlite3Error as e:
             print(e)
 
-    def replaceStockData(self,conn, item):
+    def replaceStockData(self, conn, item):
         sql = '''
         INSERT or replace INTO 
         stock(id,name,d_yield,PE,price,time_to_market,classification,share_capital,IHOLD,update_time)
@@ -201,7 +215,8 @@ class Data:
             share_capital = item['share_capital']
             IHOLD = item['IHOLD']
             update_time = item['update_time']
-            curser.execute(sql, (id, name, d_yield, PE, price, time_to_market, classification, share_capital, IHOLD, update_time))
+            curser.execute(sql, (
+            id, name, d_yield, PE, price, time_to_market, classification, share_capital, IHOLD, update_time))
         except  sqlite3Error as e:
             print(e)
         conn.commit()
@@ -216,7 +231,7 @@ class Data:
         try:
             cursor.execute(sql)
             row = cursor.fetchone()
-            #print(row[0])
+            # print(row[0])
         except sqlite3Error as e:
             print(e)
         return row[0]
@@ -239,9 +254,10 @@ class Data:
     def updateCompanyInfo(self):
         import time
         for item in list(self.listInfo.values()):
-            #print(item)
+            # print(item)
             conn = self.createConnection()
-            update_time = int("{:0>4}{:0>2}{:0>2}".format(time.gmtime().tm_year,time.gmtime().tm_mon,time.gmtime().tm_mday))
+            update_time = int(
+                "{:0>4}{:0>2}{:0>2}".format(time.gmtime().tm_year, time.gmtime().tm_mon, time.gmtime().tm_mday))
             with conn:
                 self.createStockTable(conn)
                 ctype = 99
@@ -270,6 +286,7 @@ class Data:
                     item['IHOLD'] = stock[8]
                 item['update_time'] = update_time
                 self.replaceStockData(conn, item)
+
 
 """
 條件1：股本 > 50 億元以上
--- test.py
+++ test.py
@@ -2,15 +2,17 @@ from models import Spider,Data
 import time
 
 x = Spider("2330")
-x.getGrossMargin() #取得毛利率
-x.getEps() #取得eps
+#x.getGrossMargin() #取得毛利率
+y = x.getEps().values() #取得eps
+for item in list(y):
+    print(item)
 #x.getProfile() #取得基本資料
-x.gerDividend() #取得股利資料
+#x.gerDividend() #取得股利資料
 #x.getCompany() #取得上市公司名單
 #x.getPrice() #取得收盤價
 
 #Spider().getCompany()
-Data().updateCompanyInfo() #抓取所有上市公司的資料，會執行很久
+#Data().updateCompanyInfo() #抓取所有上市公司的資料，會執行很久
 
 
 conn = Data().createConnection()
@@ -20,3 +22,12 @@ with conn:
 
 print(time.gmtime())
 print(int("{:0<4}{:0<2}{:0<2}".format(time.gmtime().tm_year,time.gmtime().tm_mon,time.gmtime().tm_mday)))
+
+
+x = dict()
+x["a"] = dict()
+x["a"]["a"]=1
+if "b" in x:
+    print(x , x["b"])
+else:
+    print(x, "NULL")
